1dbfdf9ff8bfb7a8d2fd2545031478f51f75cae5
Eric Mesa <ericsbinaryworld@gmail.com>
2020-06-02 13:18:06 -0400

    changed colors and background

--- snowflake.py
+++ snowflake.py
@@ -1,9 +1,11 @@
 """This is a snowflake program made by Samuel and Daddy on June 2 2020"""
 
 import turtle
+import random
 
 slow_poke = turtle.Turtle()
 slow_poke.color("forest green")
+turtle.Screen().bgcolor("honeydew2")
 for i in range(10):
     for i in range(2):
         slow_poke.forward(100)
