cf9b8bb26a6fc85963c1ab47abf680e3cef9fb05
Eric Mesa <ericsbinaryworld@gmail.com>
2020-06-02 13:30:47 -0400

    a traditional snowflake

--- snowflake.py
+++ snowflake.py
@@ -6,12 +6,27 @@ import random
 slow_poke = turtle.Turtle()
 slow_poke.color("forest green")
 turtle.Screen().bgcolor("honeydew2")
-for i in range(10):
-    for i in range(2):
-        slow_poke.forward(100)
-        slow_poke.right(60)
-        slow_poke.forward(100)
-        slow_poke.right(120)
-    slow_poke.right(36)
+colors = ["forest green", "HotPink1", "black", "sienna4"]
+
+slow_poke.penup()
+slow_poke.forward(90)
+slow_poke.left(45)
+slow_poke.pendown()
+
+def branch():
+    for i in range(3):
+        for i in range(3):
+             slow_poke.forward(30)
+             slow_poke.backward(30)
+             slow_poke.right(45)
+        slow_poke.left(90)
+        slow_poke.backward(30)
+        slow_poke.left(45)
+    slow_poke.right(90)
+    slow_poke.forward(90)
+
+for i in range(8):
+    branch()
+    slow_poke.left(45)
 
 turtle.mainloop()
